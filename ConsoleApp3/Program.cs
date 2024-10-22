b6af4fd85a6d5c2ae635cf8a8cf98f2d819fa663
ekelemene <ksuxaskrip2007@gmail.com>
2024-10-22 14:40:00 +0300

    Add files via upload

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //try
+            //{
+            //    Console.Write("x=");
+            //    int x = int.Parse(Console.ReadLine());
+            //    int y = x < 0 ? x + 1 : x - 1;
+            //    Console.WriteLine($"y={y}");
+            //    Console.ReadLine();
+            //}
+            //catch
+            //{ 
+
+            //}
+
+            //Здание 1
+            //try
+            //{
+            //    // Дано целое число n, указывающее номер дня недели от 1 до 7. По указанному номеру n вывести название соответствующего дня недели.
+            //    Console.WriteLine("Введите номер дня недели:");
+            //    int day = int.Parse(Console.ReadLine());
+            //    switch (day)
+            //    {
+            //        case 1: Console.WriteLine("Понедельник"); break;
+            //        case 2: Console.WriteLine("Вторник"); break;
+            //        case 3: Console.WriteLine("Среда"); break;
+            //        case 4: Console.WriteLine("Четверг"); break;
+            //        case 5: Console.WriteLine("Пятница"); break;
+            //        case 6: Console.WriteLine("Суббота"); break;
+            //        case 7: Console.WriteLine("Воскресенье"); break;
+
+            //        default: Console.WriteLine("Нет такого дня недели"); break;
+            //    }
+            //}
+            //catch 
+            //{
+            //}
+            //Console.ReadLine();
+
+            //Задача 2
+            //try
+            //{
+            //    //По номеру месяца вывести название времени года.
+            //    Console.WriteLine("Введите номер месяца:");
+            //    int month = int.Parse(Console.ReadLine());
+            //    switch (month)
+            //    {
+            //        case 1: Console.WriteLine("зима"); break;
+            //        case 2: Console.WriteLine("зима"); break;
+            //        case 3: Console.WriteLine("весна"); break;
+            //        case 4: Console.WriteLine("весна"); break;
+            //        case 5: Console.WriteLine("весна"); break;
+            //        case 6: Console.WriteLine("лето"); break;
+            //        case 7: Console.WriteLine("лето"); break;
+            //        case 8: Console.WriteLine("лето"); break;
+            //        case 9: Console.WriteLine("осень"); break;
+            //        case 10: Console.WriteLine("осень"); break;
+            //        case 11: Console.WriteLine("осень"); break;
+            //        case 12: Console.WriteLine("зима"); break;
+            //        default: Console.WriteLine("Нет такого времени года"); break;
+            //    }
+            //}
+            //catch { }
+            //Console.ReadLine();
+
+            //Задание 3
+            //try
+            //{
+            //    // Дан признак транспортного средства: a – автомобиль, в – велосипед, м - мотоцикл, с – самолет, п – поезд. Вывести на экран максимальную скорость транспортного средства в зависимости от введенного признака.
+            //    Console.WriteLine("Введите транспорт:");
+            //    char tran= char.Parse(Console.ReadLine());
+            //    switch (tran) 
+            //    {
+            //        case 'а': Console.WriteLine("Скорость автомобиля = 400км/ч"); break;
+            //        case 'в': Console.WriteLine("Скорость велосипеда = 40 км/ч"); break;
+            //        case 'м': Console.WriteLine("Скорость мотоцикла = 500км/ч"); break;
+            //        case 'с': Console.WriteLine("Скорость самолета = 1000км/ч"); break;
+            //        case 'п': Console.WriteLine("Скорость поезда = 90км/ч"); break;
+            //        default: Console.WriteLine("Ошибка"); break;
+            //    }
+            //}
+            //catch { }
+            //Console.ReadLine();
+
+            //Задание 4
+            //try
+            //{
+            //    //Вывести название предмета по введенной первой букве: ф – физика, м – математика, и – история, г – география, б – биология.
+            //    Console.WriteLine("Введите урок:");
+            //    char tran = char.Parse(Console.ReadLine());
+            //    switch (tran)
+            //    {
+            //        case 'ф': Console.WriteLine("Физика"); break;
+            //        case 'м': Console.WriteLine("Математика"); break;
+            //        case 'и': Console.WriteLine("История"); break;
+            //        case 'г': Console.WriteLine("География"); break;
+            //        case 'б': Console.WriteLine("Биология"); break;
+            //        default: Console.WriteLine("Ошибка"); break;
+            //    }
+            //}
+            //catch { }
+            //Console.ReadLine();
+
+
+
+
+
+
+
+        }
+    }
+}
